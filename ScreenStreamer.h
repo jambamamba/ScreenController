13549c3c1047ce39b27d38b476f9abd863d3414f
oosman <oosman3@yahoo.com>
2021-03-06 05:38:44 +0000

    wip

--- ScreenStreamer.h
+++ ScreenStreamer.h
@@ -39,7 +39,7 @@ protected:
     std::future<void> thread_;
     QList<QScreen *> m_screens;
     SocketReader &m_socket;
-    int m_jpeg_quality_percent = 25;
+    int m_jpeg_quality_percent = 5;
     float m_scale_factor = 1;//.85;
     MouseInterface *m_mouse;
     bool m_streaming = false;
