eb50cf022c0050e904709b3bbf98967b35a86393
oosman <oosman3@yahoo.com>
2021-03-08 14:49:52 +0000

    wip

--- EventHandler.cpp
+++ EventHandler.cpp
@@ -46,8 +46,6 @@ void EventHandler::HandleCommand(const Command &pkt, uint32_t ip)
         break;
     case Command::EventType::StoppedStreaming:
         emit StoppedStreaming(ip);
-        qDebug() << "############ StoppedStreaming";
-        m_key = std::make_unique<X11Key>(parent());
         break;
     case Command::EventType::MouseMove:
         m_mouse->moveTo(pkt.u.m_mouse.m_x, pkt.u.m_mouse.m_y);
