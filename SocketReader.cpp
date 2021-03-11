513d2dd826d7aaa7e835c63024ba2c59725b111e
oosman <oosman3@gmail.com>
2021-03-11 19:12:38 +0000

    wip

--- SocketReader.cpp
+++ SocketReader.cpp
@@ -67,24 +67,22 @@ struct Stats
 {
     void Update(ssize_t frame_sz)
     {
-        {
-            std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
-            auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - m_begin).count();
-            m_total_bytes += frame_sz;
+        std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+        auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - m_begin).count();
+        m_total_bytes += frame_sz;
 
-            qDebug() << "frame# " << m_frame_counter++ << ","
-                     << frame_sz/1024
-                     << "KBytes, "
-                     << (m_frame_counter*1000/elapsed)
-                     << "fps, "
-                     << (m_total_bytes*8/elapsed)
-                     << "kbps.";
-            if(elapsed > 1000 * 60)
-            {
-                m_begin = end;
-                m_frame_counter = 0;
-                m_total_bytes = 0;
-            }
+        qDebug() << "frame# " << (m_frame_counter++) << ","
+                 << frame_sz/1024
+                 << "KBytes, "
+                 << (m_frame_counter*1000/elapsed)
+                 << "fps, "
+                 << (m_total_bytes*8/elapsed)
+                 << "kbps.";
+        if(elapsed > 1000 * 60)
+        {
+            m_begin = end;
+            m_frame_counter = 0;
+            m_total_bytes = 0;
         }
     }
     std::chrono::steady_clock::time_point m_begin = std::chrono::steady_clock::now();
